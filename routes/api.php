302b98a4c6458d7f6f1d9c568df139839c27e33f
r1eki <muhammadrakayusrifan@gmail.com>
2018-09-13 10:56:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
+Route::get('logout', 'API\UserController@logout');
 
 Route::group(['middleware' => 'auth:api'], function(){
 	Route::post('details', 'API\UserController@details');
